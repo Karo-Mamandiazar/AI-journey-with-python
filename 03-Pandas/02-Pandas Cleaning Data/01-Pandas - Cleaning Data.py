2cb7485ee51a3a3740ae88064f5d1d6b12bb0b57
Karo Mamandiazar <karomamandiazar@gmail.com>
2025-11-17 23:30:52 +0330

    Create 01-Pandas - Cleaning Data.py

---
+++ 03-Pandas/02-Pandas Cleaning Data/01-Pandas - Cleaning Data.py
@@ -0,0 +1,15 @@
+print("---=== Pandas - Cleaning Data ===---")
+
+print('--- Data Cleaning ---')
+"""
+ - Data cleaning means fixing bad data in your data set.
+ 
+ Bad data could be:
+ 
+  - Empty cells
+  - Data in wrong format
+  - Wrong data
+  - Duplicates
+  
+ In this tutorial you will learn how to deal with all of them.
+"""
